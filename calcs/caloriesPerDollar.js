bb83c3a3052e633cc1c3ac6157d2000fe018a08e
Louis Savoie <thelousavoie@gmail.com>
2021-12-13 18:03:52 -0500

    add servings and tax inputs

--- calcs/caloriesPerDollar.js
+++ calcs/caloriesPerDollar.js
@@ -1,10 +1,16 @@
 // SELECT DOM ELEMENTS
 const priceInput = document.querySelector('#priceInput');
 const caloriesInput = document.querySelector('#caloriesInput');
+const servingsInput = document.querySelector('#servingsInput');
+const taxCheckbox = document.querySelector('#taxCheckbox');
 const calcButton = document.querySelector('#calcButton');
 const resultDisplay = document.querySelector('#resultDisplay')
 
 // CLICK EVENTS
 calcButton.addEventListener('click', function() {
-  resultDisplay.innerHTML = (caloriesInput.value / priceInput.value)
+  if (taxCheckbox.checked == true) {
+    resultDisplay.innerHTML = Math.floor((caloriesInput.value * servingsInput.value) / (priceInput.value * 1.06))
+  } else {
+    resultDisplay.innerHTML = Math.floor((caloriesInput.value * servingsInput.value) / priceInput.value)
+  }
 })
