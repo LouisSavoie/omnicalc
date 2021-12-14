93e9843966677d5ed22b8e10e7e0695e03ac1750
Louis Savoie <thelousavoie@gmail.com>
2021-12-13 23:10:44 -0500

    add results history

--- calcs/caloriesPerDollar.js
+++ calcs/caloriesPerDollar.js
@@ -5,12 +5,27 @@ const servingsInput = document.querySelector('#servingsInput');
 const taxCheckbox = document.querySelector('#taxCheckbox');
 const calcButton = document.querySelector('#calcButton');
 const resultDisplay = document.querySelector('#resultDisplay')
+const resultsHistoryDisplay = document.querySelector('#resultsHistoryDisplay')
+
+const log = []
 
 // CLICK EVENTS
 calcButton.addEventListener('click', function() {
+  // Adjust Price if Tax
+  let ifTaxPrice = 0
   if (taxCheckbox.checked == true) {
-    resultDisplay.innerHTML = Math.floor((caloriesInput.value * servingsInput.value) / (priceInput.value * 1.06))
+    ifTaxPrice = Math.round(((priceInput.value * 1.06) + Number.EPSILON) * 100) / 100
   } else {
-    resultDisplay.innerHTML = Math.floor((caloriesInput.value * servingsInput.value) / priceInput.value)
+    ifTaxPrice = priceInput.value
   }
+  // Calculate
+  let result = Math.floor((caloriesInput.value * servingsInput.value) / ifTaxPrice)
+  // Add Results to Log
+  log.push({ cals:caloriesInput.value, servs: servingsInput.value, price: ifTaxPrice, res: result })
+  // Display Results
+  resultDisplay.innerHTML = result
+  resultsHistoryDisplay.innerHTML = ''
+  log.forEach(function(value) {
+    resultsHistoryDisplay.innerHTML += `<li class="list-group-item">(${value.cals}*${value.servs})/${value.price} = <strong>${value.res}</strong></li>`
+  })
 })
